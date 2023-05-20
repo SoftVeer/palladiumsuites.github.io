0046c1690dfbd01589eae67bc0429707d2357892
Lakshman Kambam <klakshman318@gmail.com>
2023-05-20 12:05:07 +0530

    fix(palladium): build files updated

---
+++ _next/static/chunks/app/(stay-listings)/layout-370bea592f00bab4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9825,2768,4901],{97155:function(){}},function(n){n.O(0,[2667,4961,1744],function(){return n(n.s=97155)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/blog/[...slug]/layout-da62b00ed4eeae49.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4901,2768,9825],{97155:function(){}},function(n){n.O(0,[2667,4961,1744],function(){return n(n.s=97155)}),_N_E=n.O()}]);
